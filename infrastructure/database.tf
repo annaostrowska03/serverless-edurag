7ea97699cd086f6cade09078adb05f3b04982a50
Anna Ostrowska <anostrowska11@wp.pl>
2026-04-27 21:40:12 +0200

    feat: add Firestore database initialization in infrastructure configuration

---
+++ infrastructure/database.tf
@@ -0,0 +1,7 @@
+# initialization of firestore database
+resource "google_firestore_database" "database" {
+  project     = var.project_id
+  name        = "(default)"
+  location_id = var.region
+  type        = "FIRESTORE_NATIVE"
+}
